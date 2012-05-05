317d0de62be791ffca2872f0a6419c6ad2cd94f8
Bryan Barnard <bmbarnard@gmail.com>
2012-05-04 22:50:08 -0500

    baseline setup

---
+++ restinfortydays/restinfortydays.domain/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restinfortydays.domain
+{
+        [Serializable]
+        public class Movie
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public decimal Rating { get; set; }
+            public string Director { get; set; }
+            public DateTime ReleaseDate { get; set; }
+            public string TagLine { get; set; }
+            public List<string> Genres { get; set; }
+        }
+}
---
+++ restinfortydays/restinfortydays.service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="restinfortydays.service.WebApiApplication" Language="C#" %>
