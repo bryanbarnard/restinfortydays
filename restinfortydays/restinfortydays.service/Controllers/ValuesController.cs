b09372ae4f4849c08e7b867b1aac221b007fc2a6
Bryan Barnard <bmbarnard@gmail.com>
2012-05-04 23:23:24 -0500

    update simple working

--- restinfortydays/restinfortydays.service/Controllers/ValuesController.cs
+++ restinfortydays/restinfortydays.service/Controllers/ValuesController.cs
@@ -9,30 +9,42 @@ namespace restinfortydays.service.Controllers
     public class ValuesController : ApiController
     {
         // GET /api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<restinfortydays.domain.Movie> Get()
         {
-            return new string[] { "value1", "value2" };
+            
+            var movie = new restinfortydays.domain.Movie();
+            movie.Director = "Sam Huntington";
+            movie.Genres = new List<String>() { "sam", "cam" };
+            movie.Id = 1;
+            movie.Rating = 2;
+            movie.Title = "UnRestful Nights";
+            movie.TagLine = "Sleep when dead";
+
+            var movies = new List<restinfortydays.domain.Movie>();
+            movies.Add(movie);
+            return movies;
+
         }
 
-        // GET /api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET /api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST /api/values
-        public void Post(string value)
-        {
-        }
+        //// POST /api/values
+        //public void Post(string value)
+        //{
+        //}
 
-        // PUT /api/values/5
-        public void Put(int id, string value)
-        {
-        }
+        //// PUT /api/values/5
+        //public void Put(int id, string value)
+        //{
+        //}
 
-        // DELETE /api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE /api/values/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
